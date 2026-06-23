972ae4e878df447221159486e7659fc3f0b2547f
vinirex7 <249699711+vinirex7@users.noreply.github.com>
2026-06-23 13:41:19 -0300

    Export top4 strategy

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,7 +1,9 @@
 from src.strategies.momentum_macro import MomentumMacroStrategy
 from src.strategies.rebalance_anticipation import RebalanceAnticipationStrategy
+from src.strategies.top4_rotation import Top4UTILRotationStrategy
 
 __all__ = [
     "MomentumMacroStrategy",
     "RebalanceAnticipationStrategy",
+    "Top4UTILRotationStrategy",
 ]
